0fedb12593dad610748cdb5fbd707da8e7181c7e
Gianluca Gattuso <tua_email@esempio.com>
2026-03-01 12:16:12 +0100

    part 3 deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
-
+app.use(express.static('dist'))
 let notes = [
   {
     id: "1",
@@ -91,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
